27f7df2e11724476cc5fa43d5db788474052f1b3
Huy Tuấn <216107066+Tundayne2003@users.noreply.github.com>
2025-06-18 01:33:45 +0700

    save HomeMain

--- src/Pages/HomePage/HomeMain.tsx
+++ src/Pages/HomePage/HomeMain.tsx
@@ -3,42 +3,39 @@ import "../css/HomeMain.css"; // Sửa lại đường dẫn cho đúng
 
 // Main App component
 export default function App() {
-    return (
-      <div className="top-bar-container">
-        {/* Announcement Banner */}
-        <div className="announcement-banner">
-          <p>
-            MỚI! Bạn đang lập kế hoạch cho các sự kiện kinh doanh? Đăng ký gói
-            Paperless Pro và tiết kiệm 10% với mã PRO10.{" "}
-            <a href="#">Tìm hiểu thêm.</a>
-          </p>
-        </div>
+  return (
+    <div className="top-bar-container">
+      {/* Announcement Banner */}
+      <div className="announcement-banner">
+        <h3>
+          MỚI! Bạn đang lập kế hoạch cho các sự kiện kinh doanh? Đăng ký gói
+          Paperless Pro và tiết kiệm 10% với mã PRO10.{" "}
+          <a href="#">Tìm hiểu thêm.</a>
+        </h3>
+      </div>
 
-        {/* Header Row */}
-        <div className="header-row">
-          <div className="header-left">
-            <div className="logo">LOGO</div>
-            <a href="#" className="pro-link">
-              CHUYÊN NGHIỆP
-            </a>
-          </div>
-          <div className="header-right">
-            <input
-              type="text"
-              placeholder="Tìm kiếm"
-              className="search-input"
-            />
-            <a href="#" className="auth-link">
-              Đăng nhập
-            </a>
-            <a href="#" className="auth-link">
-              Đăng ký
-            </a>
-          </div>
+      {/* Header Row */}
+      <div className="header-row">
+        <div className="header-left">
+          <div className="logo">LOGO</div>
+          <a href="#" className="pro-link">
+            CHUYÊN NGHIỆP
+          </a>
         </div>
+        <div className="header-right">
+          <input type="text" placeholder="Tìm kiếm" className="search-input" />
+          <a href="#" className="auth-link">
+            Đăng nhập
+          </a>
+          <a href="#" className="auth-link">
+            Đăng ký
+          </a>
+        </div>
+      </div>
 
-        {/* Navigation Bar */}
-        <div className="navigation-bar">
+      {/* Navigation Bar */}
+      <div className="navigation-bar">
+        <div className="nav-bar-content">
           <nav>
             <ul className="nav-links">
               <li>
@@ -60,5 +57,6 @@ export default function App() {
           </nav>
         </div>
       </div>
-    );
+    </div>
+  );
 }
